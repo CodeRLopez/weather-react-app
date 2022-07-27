32b044591623f3194b186a939e790626ee096212
CodeRLopez <rlopez@nicarao.agency>
2022-07-27 16:25:00 -0600

    Agregue la carpeta de assets y estableci el primer background

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React from "react";
 
+const api = {
+  key: "99233ef3d0910a70007b94df7d46f42e",
+  base: "http://api.openweathermap.org/data/2.5/"
+}
+
 function App() {
   return (
     <div className="App">
-      hola mundo
+      <main>
+        <div className="search-box">
+          <input 
+            type="text"
+            className="search-bar"
+            placeholder="Search..."
+          />
+        </div>
+      </main>
     </div>
   );
 }
